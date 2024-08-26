56abe464e188b5906cbc513932124b34fce75a57
Vaibhavi Kumari <73634081+vaibhavi-16@users.noreply.github.com>
2024-08-26 10:02:43 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -76,23 +76,22 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'your_db_name',
-#         'USER': 'your_db_user',
-#         'PASSWORD': 'your_db_password',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
+DATABASES = {
+ 'default': {
+        'ENGINE': 'psqlextra.backend',
+        'NAME': 'newwwdb',
+        'USER': 'iaaamuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # JWT Authentication settings
 REST_FRAMEWORK = {
